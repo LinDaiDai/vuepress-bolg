54bb47ea66b3ebdfbe35c8da40674ec23bfe2dd5
wangpl <wangpl@chexindata.com>
2019-09-21 18:03:11 +0800

    数据结构-二叉树

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,82 +1,88 @@
-var config = require('./config/sidebar'); 
 module.exports = {
-  title: "LinDaiDai",
-  description: "this is LinDaiDai blog'",
-  head: [["link", { rel: "icon", href: `/logo.png` }]],
-  base: "/vuepress-bolg/",// 设置站点根路径
-  dest: "./dist",// 设置输出目录
-  repo: 'https://github.com/LinDaiDai/vuepress-bolg', // 添加 github 链接
-  themeConfig: {
-    nav: require('./nav/zh'),
-    sidebar: {
-      '/guide/': genSidebarConfig('Guide'),
-      '/webNotes/primary/': getSidebarPrimary(),
-      '/webNotes/advanced/': getSidebarAdvanced('前端进阶')
+    title: "LinDaiDai",
+    description: "this is LinDaiDai blog'",
+    head: [
+        ["link", { rel: "icon", href: `/logo.png` }]
+    ],
+    base: "/docs/", // 设置站点根路径
+    dest: "./dist", // 设置输出目录
+    repo: 'https://github.com/LinDaiDai/docs', // 添加 github 链接
+    themeConfig: {
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        nav: require('./nav/zh'),
+        sidebar: {
+            '/webNotes/primary/': primary(),
+            '/webNotes/advanced/': advanced(),
+            '/dataStructure/': dataStructure(),
+        },
     },
-    lastUpdated: 'Last Updated'
-  },
-
-  markdown: {
-    // options for markdown-it-anchor
-    anchor: { permalink: false },
-    config: md => {
-      md.use(require("markdown-it-katex"));
+    markdown: {
+        // options for markdown-it-anchor
+        anchor: { permalink: false },
+        config: md => {
+            md.use(require("markdown-it-katex"));
+        }
     }
-  }
 };
 
-function genSidebarConfig (title) {
-  return [
-    {
-      title,
-      collapsable: false,
-      children: [
+function primary() {
+    return [
         '',
-        'getting-started',
-        'customize',
-        'advanced',
-      ]
-    }
-  ]
+        {
+            title: 'HTML',
+            children: [
+                'HTML/',
+                'HTML/tag.md',
+                'HTML/form.md',
+            ]
+        },
+        {
+            title: 'CSS',
+            children: [
+                'CSS/',
+                'CSS/baseCSS.md',
+                'CSS/pseudo.md'
+            ]
+        },
+        {
+            title: 'JavaScript',
+            children: [
+                'JavaScript/baseJS.md',
+                'JavaScript/ajax.md',
+                'JavaScript/this.md',
+                'JavaScript/prototype.md'
+            ]
+        }
+    ]
 }
-function getSidebarPrimary() {
-  return [
-      '',
-      {
-          title: 'HTML',
-          children: [
-              'HTML/',
-              'HTML/tag.md',
-              'HTML/form.md',
-          ]
-      },
-      {
-          title: 'CSS',
-          children: [
-              'CSS/',
-              'CSS/baseCSS.md',
-              'CSS/pseudo.md'
-          ]
-      },
-      {
-          title: 'JavaScript',
-          children: [
-              'JavaScript/baseJS.md',
-              'JavaScript/ajax.md',
-              'JavaScript/this.md',
-              'JavaScript/prototype.md'
-          ]
-      }
-  ]
+
+function advanced() {
+    return [{
+        title: '前端进阶',
+        children: [
+            'interview/interview1.md'
+        ]
+    }]
 }
-function getSidebarAdvanced(title) {
-  return [
-    {
-      title,
-      children: [
-        '',
-        'interview/'
-      ]
-    }
-  ]
+
+function dataStructure() {
+    return [
+        '/dataStructure/',
+        {
+            title: '二叉树',
+            children: [
+                '/dataStructure/二叉树/二叉树概述',
+                '/dataStructure/二叉树/二叉树的遍历',
+                '/dataStructure/二叉树/前序遍历',
+                '/dataStructure/二叉树/中序遍历',
+                '/dataStructure/二叉树/后序遍历',
+                '/dataStructure/二叉树/重建二叉树',
+                '/dataStructure/二叉树/根据前序、中序求后序',
+                '/dataStructure/二叉树/判断是否子树',
+                '/dataStructure/二叉树/二叉树的镜像',
+                '/dataStructure/二叉树/二叉搜索树的后序遍历序列'
+            ]
+        }
+    ]
 }
--- docs/.vuepress/nav/zh.js
+++ docs/.vuepress/nav/zh.js
@@ -1,18 +1,19 @@
 module.exports = [
     { text: "Home", link: "/" },
-    { text: "前端笔记", 
+    {
+        text: "前端笔记",
         items: [
             { text: '新手入门', link: '/webNotes/primary/' },
             { text: '前端进阶', link: '/webNotes/advanced/' }
         ]
     },
+    { text: '数据结构', link: '/dataStructure/' },
     { text: "Projects", link: "/projects/" },
-    { text: "Guide", link: "/guide/" },
-    { 
+    {
         text: 'Link',
         items: [
-            { text: 'GitHub', link: 'https://github.com/LinDaiDai'},
-            { text: 'HomePage', link: 'https://lindaidai.github.io/myHomePage/'}
+            { text: 'GitHub', link: 'https://github.com/LinDaiDai' },
+            { text: 'HomePage', link: 'https://lindaidai.github.io/myHomePage/' }
         ]
     }
 ]
